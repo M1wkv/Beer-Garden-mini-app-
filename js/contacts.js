0e792c2106b321c204bd79190454597f681c3b3f
M1wkv <alexandermuwka@gmail.com>
2026-04-25 15:52:46 +0500

    Add files via upload

---
+++ js/contacts.js
@@ -0,0 +1,15 @@
+function openMap() {
+  window.open("https://maps.google.com/?q=Ташкент", "_blank");
+}
+
+function openInstagram() {
+  window.open("https://instagram.com/", "_blank");
+}
+
+function openTelegram() {
+  window.open("https://t.me/", "_blank");
+}
+
+function callPhone() {
+  window.location.href = "tel:+998901234567";
+}
